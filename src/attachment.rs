6353e1cf774eddba8a03600dbb6decedf91bdfa9
Theo Paris <theo@tinted.dev>
2024-12-05 00:36:34 -0800

    chore: initial commit

---
+++ src/attachment.rs
@@ -0,0 +1,184 @@
+use crate::{gemini::GeminiClient, Claide};
+use anyhow::Context;
+use mime::Mime;
+use reqwest::{header::CONTENT_TYPE, Url};
+use std::borrow::Cow;
+
+const DEFAULT_MIME: &str = "text/plain";
+const DEFAULT_FILE_NAME: &str = "file.txt";
+
+fn sanitize_content_type(
+	content_type: Cow<'_, str>,
+) -> anyhow::Result<Cow<'static, str>> {
+	let mime = content_type.parse::<Mime>().context("parsing mime")?;
+
+	let mut sanitized_content_type = Cow::Owned(format!(
+		"{}/{}",
+		mime.type_().as_str(),
+		mime.subtype().as_str()
+	));
+
+	if sanitized_content_type == "application/rls-services" {
+		sanitized_content_type = DEFAULT_MIME.into();
+	}
+
+	tracing::debug!(
+		"resolved mime {sanitized_content_type} from {content_type:?}"
+	);
+
+	Ok(sanitized_content_type)
+}
+
+#[derive(Debug)]
+pub struct AttachmentContent<'a> {
+	content_type: Cow<'static, str>,
+	file_name: Option<&'a str>,
+	bytes: Vec<u8>,
+}
+
+impl Default for AttachmentContent<'_> {
+	fn default() -> Self {
+		Self {
+			content_type: DEFAULT_FILE_NAME.into(),
+			file_name: None,
+			bytes: "[empty]".into(),
+		}
+	}
+}
+
+impl<'a> AttachmentContent<'a> {
+	fn new(
+		content_type: Option<Cow<'a, str>>,
+		file_name: Option<&'a str>,
+		bytes: Vec<u8>,
+	) -> anyhow::Result<Self> {
+		Ok(Self {
+			content_type: sanitize_content_type(
+				content_type.unwrap_or_else(|| DEFAULT_MIME.into()),
+			)?,
+			file_name,
+			bytes,
+		})
+	}
+
+	async fn upload(self, gemini: &GeminiClient) -> anyhow::Result<String> {
+		let file_name = self.file_name.unwrap_or(DEFAULT_FILE_NAME);
+
+		tracing::info!(
+			"uploading to gemini: {} - {}",
+			file_name,
+			self.content_type
+		);
+
+		let content_size = self.bytes.len() as u32;
+
+		let url = gemini
+			.create_file(file_name, content_size, &self.content_type)
+			.await?;
+
+		gemini.upload_file(url, content_size, self.bytes).await
+	}
+}
+
+/// Registered and uploaded file reference
+#[derive(Debug, Clone)]
+pub struct GeminiAttachment {
+	pub uri: String,
+	pub content_type: Cow<'static, str>,
+}
+
+/// Uploadable to gemini
+pub trait GeminiUpload {
+	/// Download file data
+	async fn fetch_content(
+		&self,
+		claide: &Claide,
+	) -> anyhow::Result<AttachmentContent>;
+
+	/// Upload this to gemini
+	async fn upload_into_gemini(
+		&self,
+		claide: &Claide,
+	) -> anyhow::Result<GeminiAttachment> {
+		let content = self
+			.fetch_content(claide)
+			.await
+			.inspect_err(|err| tracing::warn!("fetch failed: {err}"))
+			.unwrap_or_default();
+		let content_type = content.content_type.clone();
+		let uri = content.upload(&claide.gemini).await?;
+
+		Ok(GeminiAttachment { uri, content_type })
+	}
+}
+
+impl GeminiUpload for serenity::all::Attachment {
+	async fn fetch_content(
+		&self,
+		_claide: &Claide,
+	) -> anyhow::Result<AttachmentContent> {
+		let file_name = &self.filename;
+		let content_type = self.content_type.as_deref().map(Cow::Borrowed);
+
+		tracing::info!("downloading from discord: {}: {}", file_name, self.id);
+		let bytes = self.download().await?;
+
+		AttachmentContent::new(content_type, Some(file_name), bytes)
+	}
+}
+
+impl GeminiUpload for Url {
+	async fn fetch_content(
+		&self,
+		claide: &Claide,
+	) -> anyhow::Result<AttachmentContent> {
+		let file_name = self.path_segments().and_then(|path| path.last());
+
+		tracing::info!("downloading from url: {file_name:?}: {self}");
+		let resp = claide
+			.http_client
+			.get(self.clone())
+			.send()
+			.await?
+			.error_for_status()?;
+
+		let content_type = resp
+			.headers()
+			.get(CONTENT_TYPE)
+			.and_then(|v| v.to_str().ok())
+			.map(ToOwned::to_owned)
+			.map(Cow::Owned);
+
+		AttachmentContent::new(
+			content_type,
+			file_name,
+			resp.bytes().await?.to_vec(),
+		)
+	}
+}
+
+pub enum Attachment {
+	Discord(serenity::all::Attachment),
+	Url(Url),
+}
+
+impl Attachment {
+	pub fn url(&self) -> &str {
+		match self {
+			Self::Discord(attachment) => &attachment.url,
+			Self::Url(url) => url.as_str(),
+		}
+	}
+}
+
+impl GeminiUpload for Attachment {
+	async fn fetch_content(
+		&self,
+		claide: &Claide,
+	) -> anyhow::Result<AttachmentContent> {
+		match self {
+			Attachment::Discord(discord) => discord.fetch_content(claide).await,
+			Attachment::Url(url) => url.fetch_content(claide).await,
+		}
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,257 @@
+#![allow(dead_code)]
+use attachment::{Attachment, GeminiAttachment, GeminiUpload};
+use config::{ClydeConfig, Config};
+use dashmap::DashMap;
+use futures::StreamExt;
+use gemini::{
+	GeminiClient, GeminiMessage, GeminiPart, GeminiRequest, GeminiRole,
+	GeminiSafetySetting, GeminiSafetyThreshold, GeminiSystemPart,
+};
+use mime::Mime;
+use regex::Regex;
+use reqwest::Url;
+use serenity::{
+	all::{CreateAttachment, CreateMessage, Message, Settings},
+	async_trait,
+	prelude::*,
+};
+use std::{sync::LazyLock, time::Duration};
+
+mod attachment;
+mod config;
+mod gemini;
+
+static REGEX_URL: LazyLock<Regex> =
+	LazyLock::new(|| Regex::new(r"\bhttps://\S+").unwrap());
+
+struct Claide {
+	config: ClydeConfig,
+	gemini: GeminiClient,
+	seen: DashMap<String, GeminiAttachment>,
+	http_client: reqwest::Client,
+}
+
+impl Claide {
+	async fn process_message(
+		&self,
+		context: Context,
+		message: Message,
+	) -> anyhow::Result<()> {
+		let current_user_id = context.cache.current_user().id;
+
+		if message.author.id == current_user_id {
+			tracing::debug!("ignored self-message");
+
+			return Ok(());
+		}
+
+		if !message.mentions_me(&context).await? {
+			tracing::debug!("ignored non-mention");
+
+			return Ok(());
+		}
+
+		let previous_messages = {
+			let Some(cached_messages) =
+				context.cache.channel_messages(message.channel_id)
+			else {
+				anyhow::bail!("no channel messages");
+			};
+
+			let mut messages = cached_messages.values().collect::<Vec<_>>();
+
+			messages.sort_unstable_by(|a, b| a.id.cmp(&b.id));
+
+			let mut previous_messages = Vec::with_capacity(messages.len());
+			for message in messages {
+				let (user, role) = if message.author.id == current_user_id {
+					("Blinky", GeminiRole::Model)
+				} else {
+					let user = message
+						.author
+						.global_name
+						.as_deref()
+						.unwrap_or(&message.author.name);
+
+					(user, GeminiRole::User)
+				};
+
+				let mut attachments = Vec::new();
+
+				let content = &message.content;
+				attachments.extend(
+					REGEX_URL
+						.find_iter(content)
+						.map(|m| m.as_str())
+						.filter_map(|s| Url::try_from(s).ok())
+						.filter(|url| {
+							self.config.whitelisted_domains.url_matches(url)
+						})
+						.map(Attachment::Url),
+				);
+				attachments.extend(
+					message
+						.attachments
+						.iter()
+						.filter(|attachment| {
+							attachment
+								.content_type
+								.as_deref()
+								.and_then(|content_type| {
+									content_type.parse::<Mime>().ok()
+								})
+								.is_some_and(|mime| {
+									gemini::is_supported_mime(&mime)
+								})
+						})
+						.cloned()
+						.map(Attachment::Discord),
+				);
+
+				previous_messages.push((
+					role,
+					format!("{user}: {content}"),
+					attachments,
+				));
+			}
+
+			previous_messages
+		};
+
+		let mut request = GeminiRequest::default();
+
+		let system = ["You are a discord user named Blinky"].join(" ");
+
+		request
+			.system_instruction
+			.parts
+			.push(GeminiSystemPart { text: system });
+
+		let settings = [
+			GeminiSafetySetting::Harassment,
+			GeminiSafetySetting::HateSpeech,
+			GeminiSafetySetting::SexuallyExplicit,
+			GeminiSafetySetting::DangerousContent,
+			GeminiSafetySetting::CivicIntegrity,
+		];
+
+		let settings =
+			settings.map(|setting| (setting)(GeminiSafetyThreshold::BlockNone));
+
+		request.safety_settings.extend(settings);
+
+		for (role, text, attachments) in previous_messages {
+			let attachment =
+				attachments.into_iter().map(|attachment| async move {
+					anyhow::Ok(
+						match self.seen.entry(attachment.url().to_string()) {
+							dashmap::Entry::Occupied(occupied) => {
+								occupied.get().clone()
+							}
+							dashmap::Entry::Vacant(vacant) => {
+								let pair =
+									attachment.upload_into_gemini(self).await?;
+
+								vacant.insert(pair.clone());
+
+								pair
+							}
+						},
+					)
+				});
+
+			let iter = futures::stream::iter(attachment)
+				.buffered(3)
+				.collect::<Vec<_>>()
+				.await
+				.into_iter()
+				.flatten()
+				.map(GeminiPart::from);
+
+			let mut parts = vec![GeminiPart::from(text)];
+
+			parts.extend(iter);
+
+			request.contents.push(GeminiMessage::new(role, parts));
+		}
+
+		if request.contents.is_empty() {
+			anyhow::bail!("request is empty");
+		}
+
+		tracing::debug!("send request: {request:#?}");
+
+		let content = match self.gemini.generate(request).await {
+			Ok(content) => content,
+			Err(error) => {
+				let mut builder = CreateMessage::new();
+				builder = builder.content(format!(
+					"<@{}> fix ```\n{}```",
+					self.config.owner_id, error
+				));
+
+				message.channel_id.send_message(&context, builder).await?;
+
+				return Ok(());
+			}
+		};
+
+		let content = content.trim();
+		let content = content.strip_prefix("Blinky:").unwrap_or(content).trim();
+
+		if content.is_empty() {
+			anyhow::bail!("response is empty");
+		}
+
+		let mut builder = CreateMessage::new();
+
+		if content.chars().count() > 1950 {
+			builder = builder
+				.add_file(CreateAttachment::bytes(content, "message.txt"));
+		} else {
+			builder = builder.content(content);
+		}
+
+		message.channel_id.send_message(&context, builder).await?;
+
+		Ok(())
+	}
+}
+
+#[async_trait]
+impl EventHandler for Claide {
+	async fn message(&self, context: Context, message: Message) {
+		if let Err(error) = self.process_message(context, message).await {
+			tracing::error!("process_message: {error:?}");
+		}
+	}
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+	dotenvy::dotenv().ok();
+	tracing_subscriber::fmt::init();
+
+	let config = Config::read("clyde.toml")?;
+	let mut cache_settings = Settings::default();
+
+	cache_settings.max_messages = 500;
+	cache_settings.time_to_live = Duration::from_secs(24 * 60 * 60);
+
+	let mut client = Client::builder(
+		config.discord.token,
+		GatewayIntents::MESSAGE_CONTENT | GatewayIntents::GUILD_MESSAGES,
+	)
+	.cache_settings(cache_settings)
+	.event_handler(Claide {
+		config: config.clyde,
+		gemini: GeminiClient::new(config.gemini.token),
+		seen: DashMap::new(),
+		http_client: reqwest::Client::new(),
+	})
+	.await?;
+
+	client.start().await?;
+
+	Ok(())
+}
